193467f8e785acd31e2f97680eb51be34057817e
mohammadRcat <amarmailcat@gmail.com>
2022-07-26 02:09:13 +0600

    another commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({ cart }) => {
     return (
-        <div>
+        <div className='cart'>
             <h3>Order Summery</h3>
             <p>Selected Items:{cart.length}</p>
         </div>
